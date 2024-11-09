8fc94a0fc3550ce3bdffada7329873a64a8122cd
Thomas Humphreys <thomasehumphreys@gmail.com>
2024-11-09 17:49:48 +0700

    fix: navigation bug

--- webapp/src/icpos_frontend/pages/start/StartPage.tsx
+++ webapp/src/icpos_frontend/pages/start/StartPage.tsx
@@ -16,12 +16,19 @@ export default function StartPage(): ReactNode {
   const address = params.get("address") || "";
   const amount = params.get("amount") || "0";
 
-
   // If the merchant state is initialized it means that the backend icpos actor is available
   if (merchantState.initialized) {
     // If the merchant is initialized, navigate to the merchant page
     if (merchantState.merchant) {
-      return <Navigate to={`/merchant?address=${address}&amount=${amount}`} />;
+      return (
+        <Navigate
+          to="/merchant"
+          search={{
+            address: address,
+            amount: amount,
+          }}
+        />
+      );
     }
     // Otherwise, navigate to the config page to create a merchant
     return <Navigate to="/initial-config" />;
